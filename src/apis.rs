3e1c2ee2fb9afd154174c6b83abdc6960c7a0aa0
Siddhesh Kanawade <85673539+SiddheshKanawade@users.noreply.github.com>
2022-08-27 07:24:16 +0530

    feat: Add apis, main.rs and enable clippy check (#3)
    * feat: middleware, model
    
    * feat: apis, main.rs, enable clippy check
    
    * fix: clippy errors
    
    * feat: added license

--- src/apis.rs
+++ src/apis.rs
@@ -1,1 +1,116 @@
+// Copyright 2022 The casbin Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
+use crate::models::*;
+use crate::schema::*;
+use diesel::pg::PgConnection;
+use diesel::prelude::*;
+use poem::web::{Data, Json};
+use poem::{handler, web::Path};
+use std::ops::DerefMut;
+use std::sync::{Arc, Mutex};
+
+#[handler]
+pub fn hello(user: Data<&User>) -> String {
+    format!("hello: {}", user.name)
+}
+
+#[handler]
+pub fn get_users(mut conn: Data<&Arc<Mutex<PgConnection>>>) -> Json<Vec<User>> {
+    let results = users::table
+        .load::<User>(conn.0.lock().unwrap().deref_mut())
+        .expect("Error loading users");
+    Json(results)
+}
+
+#[handler]
+pub fn get_todos(mut conn: Data<&Arc<Mutex<PgConnection>>>) -> Json<Vec<Todo>> {
+    let results = todos::table
+        .load::<Todo>(conn.0.lock().unwrap().deref_mut())
+        .expect("Error loading todos");
+    Json(results)
+}
+
+#[handler]
+pub fn get_todo(
+    user: Data<&User>,
+    Path(todo_id): Path<i32>,
+    mut conn: Data<&Arc<Mutex<PgConnection>>>,
+) -> Json<Todo> {
+    let mut result = Todo::find_by_id(todo_id, user.0, conn.0.lock().unwrap().deref_mut()).unwrap();
+    Json(result)
+}
+
+#[handler]
+pub fn update_todo(
+    user: Data<&User>,
+    Path(todo_id): Path<i32>,
+    mut req: Json<NewTodo>,
+    mut conn: Data<&Arc<Mutex<PgConnection>>>,
+) -> Json<Todo> {
+    let _ = Todo::find_by_id(todo_id, user.0, conn.0.lock().unwrap().deref_mut()).unwrap();
+    req.user_id = user.id;
+    let _ = Todo::update(todo_id, req.0, conn.0.lock().unwrap().deref_mut()).unwrap();
+    let mut result = Todo::find_by_id(todo_id, user.0, conn.0.lock().unwrap().deref_mut());
+    Json(result.unwrap())
+}
+
+#[handler]
+pub fn delete_todo(
+    user: Data<&User>,
+    Path(todo_id): Path<i32>,
+    mut conn: Data<&Arc<Mutex<PgConnection>>>,
+) -> Json<usize> {
+    let _ = Todo::find_by_id(todo_id, user.0, conn.0.lock().unwrap().deref_mut()).unwrap();
+    let result = Todo::delete(todo_id, conn.0.lock().unwrap().deref_mut());
+    Json(result.unwrap())
+}
+
+#[handler]
+pub fn get_self_todos(
+    user: Data<&User>,
+    mut conn: Data<&Arc<Mutex<PgConnection>>>,
+) -> Json<Vec<Todo>> {
+    let results = User::find_self_todos(user.id, conn.0.lock().unwrap().deref_mut()).unwrap();
+    Json(results)
+}
+
+#[handler]
+pub fn create_todo(
+    user: Data<&User>,
+    mut req: Json<NewTodo>,
+    mut conn: Data<&Arc<Mutex<PgConnection>>>,
+) -> Json<Todo> {
+    req.user_id = user.id;
+    let result = Todo::insert(req.0, conn.0.lock().unwrap().deref_mut()).unwrap();
+    Json(result)
+}
+
+#[handler]
+pub fn get_user_todos(
+    Path(name): Path<String>,
+    mut conn: Data<&Arc<Mutex<PgConnection>>>,
+) -> Json<Vec<Todo>> {
+    let user = User::find_by_name(name, conn.0.lock().unwrap().deref_mut());
+    if user.is_err() {
+        return Json(vec![]);
+    }
+    let user = user.unwrap();
+    if user.is_none() {
+        return Json(vec![]);
+    }
+    let results =
+        User::find_self_todos(user.unwrap().id, conn.0.lock().unwrap().deref_mut()).unwrap();
+    Json(results)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,44 @@
-#[allow(unused_variables)]
+// Copyright 2022 The casbin Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#[macro_use]
+extern crate diesel;
+
 use std::env;
+use std::sync::{Arc, Mutex};
 
 use diesel::pg::PgConnection;
 use diesel::prelude::*;
 use dotenv::dotenv;
-use poem::web::Path;
-use poem::{get, handler, listener::TcpListener, Route, Server};
+use poem::{get, listener::TcpListener, EndpointExt, Route, Server};
 
 use poem_casbin_auth::casbin::function_map::key_match2;
 use poem_casbin_auth::casbin::{CoreApi, DefaultModel, FileAdapter};
 use poem_casbin_auth::CasbinService;
 
+use crate::models::*;
+use crate::schema::*;
+
+mod apis;
+mod auth;
+mod models;
+mod schema;
+
 fn establish_connection() -> PgConnection {
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
     PgConnection::establish(&database_url)
         .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
-}
-
-#[handler]
-fn hello(Path(name): Path<String>) -> String {
-    format!("hello: {}", name)
 }
 
 #[tokio::main]
@@ -28,7 +47,7 @@ async fn main() -> Result<(), std::io::Error> {
     if env::var_os("RUST_LOG").is_none() {
         env::set_var("RUST_LOG", "poem=debug");
     }
-    let _connection = establish_connection();
+    let connection = establish_connection();
 
     let m = DefaultModel::from_file("rbac_with_pattern_model.conf")
         .await
@@ -44,9 +63,26 @@ async fn main() -> Result<(), std::io::Error> {
         .write()
         .matching_fn(Some(key_match2), None);
 
-    // HOST SERVER HERE
-    let app = Route::new().at("/hello/:name", get(hello));
+    let app = Route::new()
+        .at("/hello", get(apis::hello))
+        .at("/users", get(apis::get_users))
+        .at("/todos", get(apis::get_todos))
+        .at(
+            "/todo/:id",
+            get(apis::get_todo)
+                .put(apis::update_todo)
+                .delete(apis::delete_todo),
+        )
+        .at(
+            "/user/todos",
+            get(apis::get_self_todos).post(apis::create_todo),
+        )
+        .at("/user/:name/todos", get(apis::get_user_todos))
+        .with(casbin_middleware)
+        .with(auth::BasicAuth)
+        .data(Arc::new(Mutex::new(connection)));
     Server::new(TcpListener::bind("127.0.0.1:3000"))
+        .name("poem-todo")
         .run(app)
         .await
 }
